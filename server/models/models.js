05f5057f560badc186695568c649628009e1a35d
iliasen <iliasen03@mail.ru>
2023-09-25 15:57:35 +0300

    Complete task and test in Postman

--- server/models/models.js
+++ server/models/models.js
@@ -3,9 +3,26 @@ const {DataTypes} = require('sequelize')
 
 const User = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    email: {type: DataTypes.STRING, unique: true,},
-    password: {type: DataTypes.STRING},
-    role: {type: DataTypes.STRING, defaultValue: "USER"},
+    surname: {type: DataTypes.STRING, allowNull: false},
+    name: {type: DataTypes.STRING, allowNull: false},
+    lastName: {type: DataTypes.STRING, allowNull: false},
+    birthday: {type: DataTypes.DATE, allowNull: false},
+    passportSeries: {type: DataTypes.STRING, allowNull: false},
+    passportId: {type: DataTypes.STRING, allowNull: false, unique: true},
+    whoIssuedThePassport: {type: DataTypes.STRING, allowNull: false},
+    dateOfIssue: {type: DataTypes.DATE, allowNull: false},
+    placeOfBirth: {type: DataTypes.STRING, allowNull:false},
+    registration: {type: DataTypes.STRING, allowNull:false},
+    homeTel: {type: DataTypes.STRING, unique: true},
+    mobileTel: {type: DataTypes.STRING, unique: true},
+    email: {type: DataTypes.STRING, unique: true},
+    placeOfWork: {type: DataTypes.STRING, defaultValue: "-"},
+    post: {type: DataTypes.STRING, defaultValue: "-"},
+    maritalStatus: {type: DataTypes.STRING, allowNull: false},
+    citizenship: {type: DataTypes.STRING, allowNull: false},
+    disability: {type: DataTypes.BOOLEAN, allowNull: false},
+    retiredPeople: {type: DataTypes.BOOLEAN, allowNull: false},
+    income: {type: DataTypes.INTEGER, allowNull: false}
 })
 
 module.exports = {
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,10 +4,9 @@ const UserController = require('../controllers/userController')
 const authMiddleware = require('../middleware/authMiddleware')//проверка пользователя на авторизованность
 
 router.post('/registration', UserController.registration)
-router.post('/login', UserController.login)
-router.get('/auth',authMiddleware, UserController.check)
 router.get('/:id',UserController.getOne)
-router.put('/change',authMiddleware, UserController.changePassword)
-router.delete('/delete_account/:id',authMiddleware, UserController.del_account)
+router.get('/', UserController.getAll)
+router.delete('/:id', UserController.delPerson)
+router.put("/", UserController.update)
 
 module.exports = router
